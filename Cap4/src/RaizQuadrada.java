3a8085f682fc08f5212afe95660444acdaa393d7
Maison <maisongalvao144@gmail.com>
2019-05-25 16:07:53 -0400

    Aula 04 - DSLs e o Interpreter

---
+++ Cap4/src/RaizQuadrada.java
@@ -0,0 +1,14 @@
+public class RaizQuadrada implements Expressao {
+
+    private Expressao expressao;
+
+    public RaizQuadrada(Expressao e) {
+        this.expressao = e;
+    }
+
+    @Override
+    public int avalia() {
+        return (int) Math.sqrt(expressao.avalia());
+    }
+
+}
---
+++ Cap4/src/Soma.java
@@ -0,0 +1,17 @@
+public class Soma implements Expressao {
+
+    private Expressao esquerda;
+    private Expressao direita;
+
+    public Soma(Expressao esquerda, Expressao direita) {
+        this.esquerda = esquerda;
+        this.direita = direita;
+    }
+
+    @Override
+    public int avalia() {
+        int resultadoDaEsquerda = esquerda.avalia();
+        int resultadoDaDireita = direita.avalia();
+        return resultadoDaEsquerda + resultadoDaDireita;
+    }
+}
